241e77080afc70208dbae732b207f8a7003c45ac
Asher Merrill <asher.s.merrill@gmail.com>
2025-04-16 10:28:58 -0600

    layer: use naturally-shaped weights in LocallyConnected2d

--- neuralpde/layer.py
+++ neuralpde/layer.py
@@ -21,14 +21,14 @@ class LocallyConnected2d(nn.Module):
         Initializes a locally-connected 2d layer.
 
         This is a simple implementation that I can expand later if I want or need.
-        
+
         See [the PyTorch docs on `torch.nn.Unfold`](https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html)
         for a more thorough description of these parameters and how they work.  In particular, I've tried
         to reuse the notational convention described in that documentation here.
 
         Note: Currently, only 4-D input tensors (batched image-like tensors) are supported,
         as per `nn.Unfold` and `nn.functional.unfold`.
-        
+
         Args:
             in_channels         : Number of channels in the input image (e.g., 3 for RGB).
             out_channels        : Number of output channels (number of filters per spatial location).
@@ -54,15 +54,19 @@ class LocallyConnected2d(nn.Module):
             )
         )
         """ Output spatial shape """
+
+        # Total number of blocks from unfold (unused, but also helped me write this in the first place)
+        # I want to keep this here to prevent this from descending into the arcane
+        # I.e., the process is important!
         self.L = reduce(lambda x, y: x * y, self.out_spatial_size)
         """ Total number of blocks """
-        
+
         self._block_size = self.in_channels * self.kernel_size**len(self.in_spatial_shape)
         """ Number of elements in each block """
-        
-        self.weight = nn.Parameter(torch.ones(self._block_size, out_channels, self.L) / self._block_size)
-        self.bias = nn.Parameter(torch.randn(out_channels, self.L))
-    
+
+        self.weight = nn.Parameter(torch.ones(out_channels, *self.out_spatial_size, self._block_size) / self._block_size)
+        self.bias = nn.Parameter(torch.randn(out_channels, *self.out_spatial_size))
+
     def forward(self, x: torch.tensor):
         """
         Note: Currently, only 4-D input tensors (batched image-like tensors) are supported,
@@ -71,7 +75,7 @@ class LocallyConnected2d(nn.Module):
 
         Args:
             x (Tensor): Input tensor with shape (batch_size, in_channels, *self.in_spatial_shape)
-             
+
         Returns:
             out (Tensor): Output tensor with shape (batch_size, out_channels, *self.out_spatial_shape)
         """
@@ -79,16 +83,16 @@ class LocallyConnected2d(nn.Module):
             batch_size = x.size(0)
 
             blocks = torch.nn.functional.unfold(x, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
-            out = torch.einsum('bil,iol->bol', blocks, self.weight) + self.bias.unsqueeze(0)
+            out = torch.einsum('bil,oli->bol', blocks, self.weight.flatten(1, -2)) + self.bias.flatten(1, -1).unsqueeze(0)
             out = out.reshape((batch_size, self.out_channels, *self.out_spatial_size))
             return out
-    
+
         elif x.ndim == 3:
             x = x.unsqueeze(0)
             batch_size = 1
 
             blocks = torch.nn.functional.unfold(x, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
-            out = torch.einsum('bil,iol->bol', blocks, self.weight) + self.bias.unsqueeze(0)
+            out = torch.einsum('bil,oli->bol', blocks, self.weight.flatten(1, -2)) + self.bias.flatten(1, -1).unsqueeze(0)
             out = out.reshape((batch_size, self.out_channels, *self.out_spatial_size))
             return out.squeeze(0)
 
